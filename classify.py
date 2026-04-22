339caec59b21f087642a133cdc6914a50ca845f2
Kevin Jacob <kevin@klara.au>
2026-04-22 19:28:31 +0930

    add: classify log structure

--- classify.py
+++ classify.py
@@ -1,11 +1,30 @@
 from processor_regex import classify_with_regex
 
 
+def classify(logs):
+    labels=[]
+    for source, log_msg in logs:
+       label=classify_log(source, log_msg)
+       labels.append((source,log_msg,label))
+    return labels
+
+def classify_log(log):
+    if(source=="LegacyCRM"):
+        pass #LLM
+    else:
+        label= classify_with_regex(log)
+        if label is None:
+            pass #BERT
+        return label
 
 
 if __name__ == "__main__":
-    logs=['User User685 logged out...','File uploaded successfully by user sadd','Account with ID chakka created by manga']
-    for log in logs:
-        print(f"Log: {log}")
-        print(f"Classified as: {classify_with_regex(log)}")
-        print("-"*50)
+    logs=[("ModernCRM", "User User685 logged out..." ),
+    ("AnalyticsEngine", "File uploaded successfully by user sadd"),
+    ("ModernHR", "Account with ID chakka created by manga"),
+    ("LegacyCRM", "User User685 logged out...")] 
+
+    classified_logs=classify(logs)
+
+    # for log in logs:
+    #     class
