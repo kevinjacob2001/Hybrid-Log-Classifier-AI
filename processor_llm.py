6623d09165b0c3b19f5d0a2cefbb84b255aee078
Kevin Jacob <kevin@klara.au>
2026-04-22 20:29:17 +0930

    add: groq integrations

--- processor_llm.py
+++ processor_llm.py
@@ -8,12 +8,23 @@ groq_api_key = os.getenv('GROQ_API_KEY')
 
 groq_client = Groq(api_key=groq_api_key)
 
+
+
+
+
 def classify_with_llm(log_message):
+
+    prompt=f'''Classify the following log message into one of the following categories name: 
+        (1) Workflow Error , (2) Deprecation Warning,
+        If you cant figure out the category, return "Unclassified".
+        Ony return the category, no other text or explanation.
+        Log message: {log_message}'''
+
     response = groq_client.chat.completions.create(
-        model="llama3-8b-8192",
-        messages=[{"role": "user", "content": log_message}]
+        model="llama-3.3-70b-versatile",
+        messages=[{"role": "user", "content": prompt}]
     )
     return response.choices[0].message.content
 
 if __name__ == "__main__":
-    print(classify_with_llm('Hello bro, chill yaa'))
+    print(classify_with_llm('Workflow failed at step 23:hello'))
